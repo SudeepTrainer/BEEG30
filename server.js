12a83d5d71972a95122cf605bb4ce7a8f19872f2
SudeepTrainer <info@icenbake.com>
2023-11-03 18:12:40 +0530

    02-11-2023

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const queryString = require('querystring')
 // const port = process.env.PORT || 1337;
 const server = http.createServer(function(req,res){
     console.log("request listener function called");
@@ -7,6 +8,8 @@ const server = http.createServer(function(req,res){
         respondText(req,res);
     } else if(req.url === "/person"){
         respondJson(req,res);
+    } else if(req.url.match(/^\/echo/)){
+        respondEcho(req,res);
     } else respondNothing(req,res);
     // res.end("Hi from the server");
     res.end();
@@ -28,4 +31,13 @@ function respondJson(req,res){
 function respondNothing(req,res){
     res.writeHead(404,"NOthing found");
     res.write("Nothing found.")
+}
+
+function respondEcho(req,res){
+    // console.log(req.url.split('?'));
+    // console.log(req.url.split('?').slice(1));
+    // console.log(req.url.split('?').slice(1).join(''));
+    // console.log(queryString.parse(req.url.split('?').slice(1).join('')));
+    const {input} = queryString.parse(req.url.split('?').slice(1).join(''))
+    res.end(input)
 }
