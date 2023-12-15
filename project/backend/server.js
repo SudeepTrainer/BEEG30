7eecd76e8eca76ce479d013af349ba15d91eff3e
SudeepTrainer <info@icenbake.com>
2023-12-15 18:05:24 +0530

    14-12-2023

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const mongoose = require('mongoose');
-const mongodbStore = require('connect-mongodb-session')(session)
-const databaseUri = 'mongodb://127.0.0.1:27017/mydb';
+// const mongodbStore = require('connect-mongodb-session')(session)
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+
+const databaseUri = 'mongodb://127.0.0.1:27017/authdb';
+
 
 async function db(){
     await mongoose.connect(databaseUri);
@@ -11,32 +15,49 @@ db()
 .then(res=>console.log("DB connected"))
 .catch(err=>console.log(`error ${err}`));
 
-const store = new mongodbStore(
-    {
-        uri:databaseUri,
-        collection:"sessions"
-    }
-)
+// define the user schema
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String
+})
+
+// create a user model
+const User = mongoose.model("User",userSchema);
+
+// const store = new mongodbStore(
+//     {
+//         uri:databaseUri,
+//         collection:"sessions"
+//     }
+// )
 const PORT = 3000;
 
 const application = express();
 
-//middleware
-application.use(session({
-    secret:"thisisasecretkeytosigncookies",
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        secure:false,
-        maxAge:60000
-    },
-    store:store
-}));
+// middleware
+application.set('view engine','ejs');
+application.use(cookieParser());
+
+// application.use(session({
+//     secret:"thisisasecretkeytosigncookies",
+//     resave:false,
+//     saveUninitialized:false,
+//     cookie:{
+//         secure:false,
+//         maxAge:60000
+//     },
+//     store:store
+// }));
 
 // Routing
-application.get("/",(req,res)=>{
-    res.send("Home page");
+application.get("/login",(req,res)=>{
+    res.render('login');
 })
+
+application.get("/register",(req,res)=>{
+    res.render('register');
+})
+
 
 //start server
 application.listen(PORT,()=>{
